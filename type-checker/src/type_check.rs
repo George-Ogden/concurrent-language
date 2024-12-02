3ca5850aa5f031ea6682653a1cca207fb26d43c3
George Ogden <george-ogden@outlook.com>
2024-12-02 17:39:58 +0000

    move type check utils out of type check nodes

--- type-checker/src/type_check.rs
+++ type-checker/src/type_check.rs
@@ -1,4 +1,4 @@
-use crate::type_check_nodes::{ParametricType, Type, TypeDefinitions, TypedExpression, TypedTuple};
+use crate::type_check_nodes::{ParametricType, Type, TypedExpression, TypedTuple};
 use crate::{
     AtomicType, AtomicTypeEnum, Definition, EmptyTypeDefinition, Expression, FunctionType,
     GenericType, GenericTypeVariable, Id, OpaqueTypeDefinition, TransparentTypeDefinition,
@@ -8,9 +8,254 @@ use counter::Counter;
 use itertools::Itertools;
 use std::cell::RefCell;
 use std::collections::{HashMap, HashSet};
-use std::fmt::{self, Debug};
+use std::fmt;
 use std::rc::Rc;
 use strum::IntoEnumIterator;
+
+type TypeReferencesIndex = HashMap<*mut ParametricType, Id>;
+
+struct TypeDefinitions(HashMap<Id, Rc<RefCell<ParametricType>>>);
+
+impl TypeDefinitions {
+    pub fn new() -> Self {
+        TypeDefinitions(HashMap::new())
+    }
+    pub fn get(&self, k: &Id) -> Option<&Rc<RefCell<ParametricType>>> {
+        self.0.get(k)
+    }
+    pub fn get_mut(&mut self, k: &Id) -> Option<&mut Rc<RefCell<ParametricType>>> {
+        self.0.get_mut(k)
+    }
+    fn references_index(&self) -> TypeReferencesIndex {
+        self.0
+            .iter()
+            .map(|(key, value)| (value.clone().as_ptr(), key.clone()))
+            .collect::<HashMap<_, _>>()
+    }
+    fn type_equality(
+        self_references_index: &TypeReferencesIndex,
+        other_references_index: &TypeReferencesIndex,
+        t1: &Type,
+        t2: &Type,
+    ) -> bool {
+        match (t1, t2) {
+            (Type::Atomic(a1), Type::Atomic(a2)) => a1 == a2,
+            (Type::Union(v1), Type::Union(v2)) => {
+                v1.len() == v2.len()
+                    && v1
+                        .iter()
+                        .sorted_by_key(|(i1, _)| *i1)
+                        .zip(v2.iter().sorted_by_key(|(i1, _)| *i1))
+                        .all(|((i1, o1), (i2, o2))| {
+                            i1 == i2
+                                && match (&o1, &o2) {
+                                    (None, None) => true,
+                                    (Some(t1), Some(t2)) => TypeDefinitions::type_equality(
+                                        self_references_index,
+                                        other_references_index,
+                                        t1,
+                                        t2,
+                                    ),
+                                    _ => false,
+                                }
+                        })
+            }
+            (Type::Instantiation(t1, i1), Type::Instantiation(t2, i2)) => {
+                self_references_index.get(&t1.as_ptr()) == other_references_index.get(&t2.as_ptr())
+                    && i1.len() == i2.len()
+                    && i1.into_iter().zip(i2.into_iter()).all(|(t1, t2)| {
+                        TypeDefinitions::type_equality(
+                            self_references_index,
+                            other_references_index,
+                            t1,
+                            t2,
+                        )
+                    })
+            }
+            (Type::Tuple(t1), Type::Tuple(t2)) => {
+                t1.len() == t2.len()
+                    && t1.iter().zip(t2.iter()).all(|(t1, t2)| {
+                        TypeDefinitions::type_equality(
+                            self_references_index,
+                            other_references_index,
+                            t1,
+                            t2,
+                        )
+                    })
+            }
+            (Type::Function(a1, r1), Type::Function(a2, r2)) => {
+                TypeDefinitions::type_equality(
+                    self_references_index,
+                    other_references_index,
+                    a1,
+                    a2,
+                ) && TypeDefinitions::type_equality(
+                    self_references_index,
+                    other_references_index,
+                    r1,
+                    r2,
+                )
+            }
+            (Type::Variable(i1), Type::Variable(i2)) => i1 == i2,
+            _ => false,
+        }
+    }
+}
+
+impl From<HashMap<Id, Rc<RefCell<ParametricType>>>> for TypeDefinitions {
+    fn from(value: HashMap<Id, Rc<RefCell<ParametricType>>>) -> Self {
+        TypeDefinitions(value)
+    }
+}
+
+impl<const N: usize> From<[(Id, Rc<RefCell<ParametricType>>); N]> for TypeDefinitions {
+    fn from(arr: [(Id, Rc<RefCell<ParametricType>>); N]) -> Self {
+        HashMap::from(arr).into()
+    }
+}
+
+impl FromIterator<(Id, ParametricType)> for TypeDefinitions {
+    fn from_iter<T: IntoIterator<Item = (Id, ParametricType)>>(iter: T) -> Self {
+        HashMap::from_iter(iter).into()
+    }
+}
+
+impl From<HashMap<Id, ParametricType>> for TypeDefinitions {
+    fn from(value: HashMap<Id, ParametricType>) -> Self {
+        value
+            .into_iter()
+            .map(|(id, type_)| (id, Rc::from(RefCell::from(type_))))
+            .collect::<HashMap<_, _>>()
+            .into()
+    }
+}
+
+impl<const N: usize> From<[(Id, ParametricType); N]> for TypeDefinitions {
+    fn from(arr: [(Id, ParametricType); N]) -> Self {
+        HashMap::from(arr).into()
+    }
+}
+
+impl From<HashMap<Id, Type>> for TypeDefinitions {
+    fn from(value: HashMap<Id, Type>) -> Self {
+        value
+            .into_iter()
+            .map(|(id, type_)| (id, type_.into()))
+            .collect::<HashMap<_, ParametricType>>()
+            .into()
+    }
+}
+
+impl<const N: usize> From<[(Id, Type); N]> for TypeDefinitions {
+    fn from(arr: [(Id, Type); N]) -> Self {
+        arr.into_iter().collect()
+    }
+}
+impl FromIterator<(Id, Type)> for TypeDefinitions {
+    fn from_iter<T: IntoIterator<Item = (Id, Type)>>(iter: T) -> Self {
+        HashMap::from_iter(iter).into()
+    }
+}
+
+impl fmt::Debug for TypeDefinitions {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let references_index = Box::new(self.references_index());
+        f.debug_map()
+            .entries(self.0.iter().map(|(key, value)| {
+                (
+                    key,
+                    DebugTypeWrapper(value.borrow().clone().type_, references_index.clone()),
+                )
+            }))
+            .finish()
+    }
+}
+
+struct DebugTypeWrapper(Type, Box<HashMap<*mut ParametricType, Id>>);
+impl fmt::Debug for DebugTypeWrapper {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let references_index = &self.1;
+        match self.0.clone() {
+            Type::Union(variants) => {
+                write!(
+                    f,
+                    "Union({:?})",
+                    variants
+                        .into_iter()
+                        .map(|(id, type_)| {
+                            (
+                                id,
+                                type_
+                                    .map(|type_| DebugTypeWrapper(type_, references_index.clone())),
+                            )
+                        })
+                        .collect_vec()
+                )
+            }
+            Type::Instantiation(rc, instances) => {
+                write!(
+                    f,
+                    "Instantation({}, {:?})",
+                    references_index
+                        .get(&rc.as_ptr())
+                        .unwrap_or(&Id::from("unknown")),
+                    instances
+                        .into_iter()
+                        .map(|type_| DebugTypeWrapper(type_, references_index.clone()))
+                        .collect_vec()
+                )
+            }
+            Type::Tuple(types) => {
+                write!(
+                    f,
+                    "Tuple({:?})",
+                    types
+                        .into_iter()
+                        .map(|type_| DebugTypeWrapper(type_, references_index.clone()))
+                        .collect_vec()
+                )
+            }
+            Type::Function(argument_type, return_type) => {
+                write!(
+                    f,
+                    "Function({:?},{:?})",
+                    DebugTypeWrapper(*argument_type, references_index.clone()),
+                    DebugTypeWrapper(*return_type, references_index.clone()),
+                )
+            }
+            type_ => write!(f, "{:?}", type_),
+        }
+    }
+}
+
+impl PartialEq for TypeDefinitions {
+    fn eq(&self, other: &Self) -> bool {
+        if self.0.keys().collect::<HashSet<_>>() != other.0.keys().collect::<HashSet<_>>() {
+            return false;
+        }
+        let self_references_index = &self.references_index();
+        let other_references_index = &other.references_index();
+        self.0
+            .keys()
+            .map(|key| (self.0.get(key), other.0.get(key)))
+            .all(|(v1, v2)| match (v1, v2) {
+                (Some(t1), Some(t2)) => {
+                    let p1 = &*&t1.borrow();
+                    let p2 = &*&t2.borrow();
+                    p1.num_parameters == p2.num_parameters
+                        && TypeDefinitions::type_equality(
+                            self_references_index,
+                            other_references_index,
+                            &p1.type_,
+                            &p2.type_,
+                        )
+                }
+                _ => false,
+            })
+    }
+}
+
+type TypeContext = HashMap<Id, ParametricType>;
 
 struct TypeChecker {}
 
@@ -240,7 +485,7 @@ impl TypeChecker {
 mod tests {
 
     use crate::{
-        type_check_nodes::{TypeDefinitions, TYPE_BOOL, TYPE_INT},
+        type_check_nodes::{TYPE_BOOL, TYPE_INT},
         Boolean, GenericTypeVariable, Integer, TupleExpression, TypeItem, TypeVariable, Typename,
         ATOMIC_TYPE_BOOL, ATOMIC_TYPE_INT,
     };
