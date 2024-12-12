efa66a58868652813e844890e152f8dca683b645
George Ogden <george-ogden@outlook.com>
2024-12-12 11:31:05 +0000

    define addition operator

---
+++ backend/include/fn/predefined.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "fn/fn.hpp"
+#include "types/predefined.hpp"
+
+struct Plus__BuiltIn : public ParametricFn<Int, Int, Int> {
+    void body() override { *ret = *std::get<0>(args) + *std::get<1>(args); }
+};
--- backend/test/fn_tests.hpp
+++ backend/test/fn_tests.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "fn.hpp"
+#include "fn/fn.hpp"
+#include "fn/predefined.hpp"
 
 #include <gtest/gtest.h>
 
@@ -13,7 +14,23 @@ GTEST_TEST(FnTests, IdentityTest) {
     int x = 5, r = 0;
     std::get<0>(id.args) = &x;
     id.ret = &r;
+    ASSERT_EQ(r, 0);
+
     id.run();
     ASSERT_EQ(r, 5);
     ASSERT_EQ(x, 5);
+}
+
+GTEST_TEST(FnTests, PlusTest) {
+    Plus__BuiltIn plus{};
+    Int x = 5, y = 10, r = 0;
+    std::get<0>(plus.args) = &x;
+    std::get<1>(plus.args) = &y;
+    plus.ret = &r;
+    ASSERT_EQ(r, 0);
+
+    plus.run();
+    ASSERT_EQ(x, 5);
+    ASSERT_EQ(y, 10);
+    ASSERT_EQ(r, 15);
 }
