97d7993ceebcc36531b3d5be88d839dc2a5106c6
George Ogden <george-ogden@outlook.com>
2025-01-12 09:22:19 +0000

    define intermediate expressions

--- lowering/src/intermediate_nodes.rs
+++ lowering/src/intermediate_nodes.rs
@@ -1,6 +1,7 @@
 use std::{cell::RefCell, rc::Rc};
 
 use from_variants::FromVariants;
+use type_checker::{Boolean, Integer};
 
 use crate::{AtomicType, Name};
 
@@ -24,3 +25,64 @@ pub struct IntermediateUnionType(Rc<RefCell<Vec<Option<IntermediateType>>>>);
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct TypeDef(pub Rc<RefCell<Vec<Option<IntermediateType>>>>);
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum IntermediateValue {
+    IntermediateBuiltIn(IntermediateBuiltIn),
+    IntermediateMemory(IntermediateMemory),
+}
+
+#[derive(Clone, Debug, FromVariants, PartialEq)]
+pub enum IntermediateBuiltIn {
+    Integer(Integer),
+    Boolean(Boolean),
+    BuiltInFn(Name, IntermediateType),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IntermediateMemory(Rc<RefCell<IntermediateExpression>>);
+
+#[derive(Clone, Debug, PartialEq, FromVariants)]
+pub enum IntermediateExpression {
+    IntermediateArgument(IntermediateType),
+    IntermediateValue(IntermediateValue),
+    IntermediateElementAccess(IntermediateElementAccess),
+    IntermediateTupleExpression(IntermediateTupleExpression),
+    IntermediateFnCall(IntermediateFnCall),
+    IntermediateCtorCall(IntermediateCtorCall),
+    IntermediateFnDef(IntermediateFnDef),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IntermediateElementAccess {
+    pub value: IntermediateValue,
+    pub idx: usize,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IntermediateTupleExpression(pub Vec<IntermediateValue>);
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IntermediateFnCall {
+    pub fn_: IntermediateValue,
+    pub args: Vec<IntermediateValue>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IntermediateCtorCall {
+    pub idx: usize,
+    pub data: Rc<RefCell<Vec<Option<IntermediateType>>>>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IntermediateAssignment(pub IntermediateMemory);
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct IntermediateFnDef {
+    argument_types: Vec<IntermediateType>,
+    statements: Vec<IntermediateStatement>,
+    return_value: IntermediateValue,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum IntermediateStatement {}
