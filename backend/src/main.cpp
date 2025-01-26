48efd886aaffca8c8e14a16a97448299f28ad03c
George Ogden <george-ogden@outlook.com>
2025-01-26 18:08:28 +0000

    add execution timer

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,11 +1,22 @@
 #include "main/main.hpp"
 #include "system/work_manager.hpp"
 
+#include <chrono>
 #include <iostream>
 
 int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+
     Main main{};
     WorkManager::run(&main);
+
+    auto end = std::chrono::high_resolution_clock::now();
+
     std::cout << main.value() << std::endl;
+
+    auto duration =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    std::cerr << "Execution time: " << duration.count() << " ns" << std::endl;
+
     return 0;
 }
