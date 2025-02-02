c634d0ebe375a0ed9f428582e92828addb60fc6a
George Ogden <george-ogden@outlook.com>
2025-02-02 19:28:57 +0000

    refactor main to use time utils for timing

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,22 +1,21 @@
 #include "main/main.hpp"
 #include "system/work_manager.hpp"
+#include "time/utils.hpp"
 
-#include <chrono>
 #include <iostream>
 
 int main() {
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start = time_utils::now();
 
     std::shared_ptr<Main> main = std::make_shared<Main>();
     WorkManager::run(main);
 
-    auto end = std::chrono::high_resolution_clock::now();
+    auto end = time_utils::now();
 
     std::cout << main->value() << std::endl;
 
-    auto duration =
-        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
-    std::cerr << "Execution time: " << duration.count() << " ns" << std::endl;
+    auto duration = time_utils::time_delta(start, end);
+    std::cerr << "Execution time: " << duration << " ns" << std::endl;
 
     return 0;
 }
