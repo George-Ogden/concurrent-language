fd14170140b38fc152764fac0065727509b61f8c
George Ogden <george-ogden@outlook.com>
2025-02-16 09:29:24 +0000

    define minimal lazy data type

--- backend/include/data_structures/lazy.hpp
+++ backend/include/data_structures/lazy.hpp
@@ -1,66 +1,23 @@
 #pragma once
 
-#include "data_structures/lock.hpp"
 #include "fn/continuation.hpp"
 #include "types/utils.hpp"
 
-#include <memory>
-#include <tuple>
-#include <type_traits>
-#include <vector>
+#include <atomic>
+#include <utility>
 
-template <typename T, typename U> class LazyPlaceholder;
-
-template <typename T> struct Lazy {
-    virtual bool done() const = 0;
-    virtual T value() const = 0;
-    virtual void add_continuation(Continuation c) = 0;
-    virtual ~Lazy() = default;
-    void update_continuation(Continuation c);
-
-    template <typename U> static auto extract_value(const U &v);
-    template <typename... Us>
-    static auto extract_value(const std::tuple<Us...> &v);
-    template <typename U>
-    static auto extract_value(const std::shared_ptr<Lazy<U>> &v);
-
-    template <typename U>
-    requires is_shared_ptr_v<U>
-    static LazyT<T> make_placeholders(U fn);
-};
-
-template <typename T> class LazyPlaceholderBase : public Lazy<T> {
-    LazyT<T> reference = nullptr;
-    Locked<std::vector<Continuation>> continuations;
+template <typename T> class Lazy {
+    T _value;
+    std::atomic<bool> done_flag;
 
   public:
-    void add_continuation(Continuation c) override;
-    void assign(LazyT<T> value);
-    bool done() const override;
-    T value() const override;
+    Lazy();
+    template <typename... Args> Lazy(Args &&...);
+    bool done() const;
+    T value() const;
+    static void update_continuation(Continuation c);
 };
 
-template <typename T, typename U>
-class LazyPlaceholder : public LazyPlaceholderBase<T> {
-    std::shared_ptr<U> reference;
-
-  public:
-    explicit LazyPlaceholder(std::shared_ptr<U> reference);
-};
-
-template <typename T> class LazyConstant : public Lazy<T> {
-    const T data;
-
-  public:
-    explicit LazyConstant();
-    explicit LazyConstant(const T &data);
-    explicit LazyConstant(T &&data);
-    template <std::size_t Index>
-    explicit LazyConstant(std::integral_constant<std::size_t, Index>);
-    template <std::size_t Index, typename U>
-    explicit LazyConstant(std::integral_constant<std::size_t, Index>,
-                          U &&value);
-    bool done() const override;
-    T value() const override;
-    void add_continuation(Continuation c) override;
-};
+template <typename T, typename... Args> LazyT<T> make_lazy(Args &&...args) {
+    return std::make_shared<Lazy<T>>(std::forward<Args>(args)...);
+}
--- backend/test/test_lazy.hpp
+++ backend/test/test_lazy.hpp
@@ -1,12 +1,6 @@
 #pragma once
 
 #include "data_structures/lazy.tpp"
-#include "data_structures/lock.tpp"
-#include "fn/continuation.tpp"
-#include "fn/operators.hpp"
-#include "system/thread_manager.tpp"
-#include "types/builtin.hpp"
-#include "types/compound.tpp"
 
 #include <gtest/gtest.h>
 
@@ -15,153 +9,15 @@
 
 class LazyConstantTest : public ::testing::Test {
   protected:
-    LazyConstant<Int> x{3};
+    LazyT<Int> x = std::make_shared<Lazy<Int>>(3);
     void SetUp() override { ThreadManager::register_self(0); }
 };
 
-TEST_F(LazyConstantTest, AlwaysDone) {
-    auto x = this->x;
-    ASSERT_TRUE(x.done());
-}
+TEST_F(LazyConstantTest, AlwaysDone) { ASSERT_TRUE(x->done()); }
 
-TEST_F(LazyConstantTest, CorrectValue) { ASSERT_EQ(x.value(), 3); }
+TEST_F(LazyConstantTest, CorrectValue) { ASSERT_EQ(x->value(), 3); }
 
-TEST_F(LazyConstantTest, UnfinishedContinuationBehaviour) {
-    std::atomic<unsigned> *remaining = new std::atomic<unsigned>{2};
-    std::atomic<unsigned> counter{1};
-    Locked<bool> *valid = new Locked<bool>{true};
-    x.add_continuation(Continuation{remaining, counter, valid});
-    ASSERT_EQ(remaining->load(std::memory_order_relaxed), 1);
-    ASSERT_EQ(counter.load(std::memory_order_relaxed), 1);
-    ASSERT_EQ(**valid, true);
-    delete remaining;
-}
-
-TEST_F(LazyConstantTest, FinishedContinuationBehaviour) {
-    std::atomic<unsigned> *remaining = new std::atomic<unsigned>{1};
-    std::atomic<unsigned> counter{1};
-    Locked<bool> *valid = new Locked<bool>{true};
-    x.add_continuation(Continuation{remaining, counter, valid});
-    ASSERT_EQ(counter.load(std::memory_order_relaxed), 2);
-    ASSERT_EQ(**valid, false);
-}
-
-TEST_F(LazyConstantTest, InvalidFinishedContinuationBehaviour) {
-    std::atomic<unsigned> *remaining = new std::atomic<unsigned>{1};
-    std::atomic<unsigned> counter{1};
-    Locked<bool> *valid = new Locked<bool>{false};
-    x.add_continuation(Continuation{remaining, counter, valid});
-    ASSERT_EQ(counter.load(std::memory_order_relaxed), 1);
-}
-
-class LazyFunctionTest : public ::testing::Test {
-  protected:
-    FnT<Int, Int> lazy_fn;
-    void SetUp() override {
-        ThreadManager::override_concurrency(1);
-        ThreadManager::register_self(0);
-        WorkManager::counters = std::vector<std::atomic<unsigned>>(1);
-        lazy_fn = std::make_shared<Increment__BuiltIn_Fn>();
-        lazy_fn->args = std::make_tuple(std::make_shared<LazyConstant<Int>>(4));
-    }
-    void TearDown() override { ThreadManager::reset_concurrency_override(); }
-};
-
-TEST_F(LazyFunctionTest, DoneLater) {
-    ASSERT_FALSE(lazy_fn->done());
-    lazy_fn->run();
-    ASSERT_TRUE(lazy_fn->done());
-}
-
-TEST_F(LazyFunctionTest, CorrectValue) {
-    lazy_fn->run();
-    ASSERT_EQ(lazy_fn->value(), 5);
-}
-
-TEST_F(LazyFunctionTest, DoneUnfinishedContinuationBehaviour) {
-    std::atomic<unsigned> *remaining = new std::atomic<unsigned>{2};
-    std::atomic<unsigned> counter{1};
-    Locked<bool> *valid = new Locked<bool>{true};
-    lazy_fn->run();
-    lazy_fn->add_continuation(Continuation{remaining, counter, valid});
-    ASSERT_EQ(remaining->load(std::memory_order_relaxed), 1);
-    ASSERT_EQ(counter.load(std::memory_order_relaxed), 1);
-    ASSERT_EQ(**valid, true);
-    delete remaining;
-}
-
-TEST_F(LazyFunctionTest, NotDoneUnfinishedContinuationBehaviour) {
-    std::atomic<unsigned> *remaining = new std::atomic<unsigned>{2};
-    std::atomic<unsigned> counter{1};
-    Locked<bool> *valid = new Locked<bool>{true};
-    lazy_fn->add_continuation(Continuation{remaining, counter, valid});
-    ASSERT_EQ(remaining->load(std::memory_order_relaxed), 2);
-    ASSERT_EQ(counter.load(std::memory_order_relaxed), 1);
-    ASSERT_EQ(**valid, true);
-    lazy_fn->run();
-    ASSERT_EQ(remaining->load(std::memory_order_relaxed), 1);
-    ASSERT_EQ(counter.load(std::memory_order_relaxed), 1);
-    ASSERT_EQ(**valid, true);
-    delete remaining;
-}
-
-TEST_F(LazyFunctionTest, DoneFinishedContinuationBehaviour) {
-    std::atomic<unsigned> *remaining = new std::atomic<unsigned>{1};
-    std::atomic<unsigned> counter{1};
-    Locked<bool> *valid = new Locked<bool>{true};
-    ASSERT_EQ(**valid, true);
-    lazy_fn->run();
-    lazy_fn->add_continuation(Continuation{remaining, counter, valid});
-    ASSERT_EQ(counter.load(std::memory_order_relaxed), 2);
-    ASSERT_EQ(**valid, false);
-}
-
-TEST_F(LazyFunctionTest, NotDoneFinishedContinuationBehaviour) {
-    std::atomic<unsigned> *remaining = new std::atomic<unsigned>{1};
-    std::atomic<unsigned> counter{1};
-    Locked<bool> *valid = new Locked<bool>{true};
-    lazy_fn->add_continuation(Continuation{remaining, counter, valid});
-    ASSERT_EQ(remaining->load(std::memory_order_relaxed), 1);
-    ASSERT_EQ(counter.load(std::memory_order_relaxed), 1);
-    ASSERT_EQ(**valid, true);
-    lazy_fn->run();
-    ASSERT_EQ(counter.load(std::memory_order_relaxed), 2);
-    ASSERT_EQ(**valid, false);
-}
-
-TEST_F(LazyFunctionTest, DoneInvalidFinishedContinuationBehaviour) {
-    std::atomic<unsigned> *remaining = new std::atomic<unsigned>{1};
-    std::atomic<unsigned> counter{1};
-    Locked<bool> *valid = new Locked<bool>{false};
-    ASSERT_EQ(**valid, false);
-    lazy_fn->run();
-    lazy_fn->add_continuation(Continuation{remaining, counter, valid});
-    ASSERT_EQ(counter.load(std::memory_order_relaxed), 1);
-}
-
-TEST_F(LazyFunctionTest, NotDoneInvalidFinishedContinuationBehaviour) {
-    std::atomic<unsigned> *remaining = new std::atomic<unsigned>{1};
-    std::atomic<unsigned> counter{1};
-    Locked<bool> *valid = new Locked<bool>{false};
-    lazy_fn->add_continuation(Continuation{remaining, counter, valid});
-    ASSERT_EQ(remaining->load(std::memory_order_relaxed), 1);
-    ASSERT_EQ(counter.load(std::memory_order_relaxed), 1);
-    ASSERT_EQ(**valid, false);
-    lazy_fn->run();
-    ASSERT_EQ(counter.load(std::memory_order_relaxed), 1);
-}
-
-TEST(LazyExtractionTest, Int) { ASSERT_EQ(Lazy<Int>::extract_value(8), 8); }
-
-TEST(LazyExtractionTest, LazyInt) {
-    LazyT<Int> ptr = std::make_shared<LazyConstant<Int>>(8);
-    ASSERT_EQ(Lazy<Int>::extract_value(ptr), 8);
-}
-
-TEST(LazyExtractionTest, LazyTuple) {
-    LazyT<TupleT<Int, TupleT<Int>>> ptr = std::make_tuple(
-        std::make_shared<LazyConstant<Int>>(6),
-        std::make_tuple(std::make_shared<LazyConstant<Int>>(10)));
-    ASSERT_EQ((Lazy<TupleT<Int, TupleT<Int>>>::extract_value(ptr)),
-              (std::make_tuple(6, std::make_tuple(10))));
+TEST(MakeLazyTest, CorrectValue) {
+    LazyT<Int> y = make_lazy<Int>(-3);
+    ASSERT_EQ(y->value(), -3);
 }
