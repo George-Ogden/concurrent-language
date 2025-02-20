b4e0906c46eb0790be22960fa9577d0e5adc6fc3
George Ogden <george-ogden@outlook.com>
2025-02-20 15:25:08 +0000

    refactor atomic shared enum, status and tests

---
+++ backend/test/test_continuation.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "fn/continuation.tpp"
+
+#include <gtest/gtest.h>
+
+#include <atomic>
+#include <memory>
+
+class ContinuationTest : public ::testing::Test {
+  protected:
+    void SetUp() override { ThreadManager::register_self(0); }
+};
+
+TEST_F(ContinuationTest, UnfinishedContinuationBehaviour) {
+    std::atomic<unsigned> *remaining = new std::atomic<unsigned>{2};
+    std::atomic<unsigned> counter{1};
+    Locked<bool> *valid = new Locked<bool>{true};
+    Continuation c{remaining, counter, valid};
+    c.update();
+    ASSERT_EQ(remaining->load(std::memory_order_relaxed), 1);
+    ASSERT_EQ(counter.load(std::memory_order_relaxed), 1);
+    ASSERT_EQ(**valid, true);
+    delete remaining;
+}
+
+TEST_F(ContinuationTest, FinishedContinuationBehaviour) {
+    std::atomic<unsigned> *remaining = new std::atomic<unsigned>{1};
+    std::atomic<unsigned> counter{1};
+    Locked<bool> *valid = new Locked<bool>{true};
+    Continuation c{remaining, counter, valid};
+    c.update();
+    ASSERT_EQ(counter.load(std::memory_order_relaxed), 2);
+    ASSERT_EQ(**valid, false);
+}
+
+TEST_F(ContinuationTest, InvalidFinishedContinuationBehaviour) {
+    std::atomic<unsigned> *remaining = new std::atomic<unsigned>{1};
+    std::atomic<unsigned> counter{1};
+    Locked<bool> *valid = new Locked<bool>{false};
+    Continuation c{remaining, counter, valid};
+    c.update();
+    ASSERT_EQ(counter.load(std::memory_order_relaxed), 1);
+}
