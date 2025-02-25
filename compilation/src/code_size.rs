a09bc8dcac28f11d6408aa87ba2092a7b59bfe64
George Ogden <george-ogden@outlook.com>
2025-02-25 17:32:32 +0000

    calculate statement sizes

--- compilation/src/code_size.rs
+++ compilation/src/code_size.rs
@@ -1,8 +1,16 @@
 use std::collections::HashMap;
+use std::ops::Add;
 
+use gcollections::ops::*;
+use interval::ops::*;
+use interval::Interval;
+use lowering::IntermediateIfStatement;
+use lowering::IntermediateMatchBranch;
+use lowering::IntermediateMatchStatement;
+use lowering::IntermediateStatement;
 use lowering::{
-    BuiltInFn, Id, IntermediateBuiltIn, IntermediateExpression, IntermediateFnCall,
-    IntermediateValue,
+    BuiltInFn, Id, IntermediateAssignment, IntermediateBuiltIn, IntermediateExpression,
+    IntermediateFnCall, IntermediateValue,
 };
 use once_cell::sync::Lazy;
 
@@ -17,6 +25,9 @@ struct CodeSizeConstants {
     fn_call_size: usize,
     ctor_call_size: usize,
     lambda_size: usize,
+    assignment_size: usize,
+    if_statement_size: usize,
+    match_statement_size: usize,
     operators: HashMap<Id, usize>,
 }
 
@@ -30,7 +41,10 @@ const CODE_SIZE_CONSTANTS: Lazy<CodeSizeConstants> = Lazy::new(|| CodeSizeConsta
     value_expression_size: 1,
     fn_call_size: 89,
     ctor_call_size: 92,
-    lambda_size: 45,
+    lambda_size: 47,
+    assignment_size: 5,
+    if_statement_size: 13,
+    match_statement_size: 17,
     operators: HashMap::from(
         [
             ("**", 10),
@@ -110,19 +124,67 @@ impl CodeSizeEstimator {
             IntermediateExpression::IntermediateLambda(_) => CODE_SIZE_CONSTANTS.lambda_size,
         }
     }
+
+    fn statement_size(statement: &IntermediateStatement) -> Interval<usize> {
+        match statement {
+            IntermediateStatement::IntermediateAssignment(assignment) => {
+                Self::assignment_size(assignment)
+            }
+            IntermediateStatement::IntermediateIfStatement(if_statement) => {
+                Self::if_statement_size(if_statement)
+            }
+            IntermediateStatement::IntermediateMatchStatement(match_statement) => {
+                Self::match_statement_size(match_statement)
+            }
+        }
+    }
+    fn assignment_size(assignment: &IntermediateAssignment) -> Interval<usize> {
+        Interval::singleton(
+            Self::expression_size(&assignment.expression) + CODE_SIZE_CONSTANTS.assignment_size,
+        )
+    }
+    fn if_statement_size(if_statement: &IntermediateIfStatement) -> Interval<usize> {
+        let condition_size = Self::value_size(&if_statement.condition);
+        let branch_sizes = Self::statements_size(&if_statement.branches.0)
+            .hull(&Self::statements_size(&if_statement.branches.1));
+        branch_sizes + condition_size + CODE_SIZE_CONSTANTS.if_statement_size
+    }
+    fn match_statement_size(match_statement: &IntermediateMatchStatement) -> Interval<usize> {
+        let subject_size = Self::value_size(&match_statement.subject);
+        let branch_sizes = match_statement
+            .branches
+            .iter()
+            .map(Self::match_branch_size)
+            .reduce(|x, y| Interval::hull(&x, &y))
+            .unwrap();
+        branch_sizes + subject_size + CODE_SIZE_CONSTANTS.match_statement_size
+    }
+    fn match_branch_size(match_branch: &IntermediateMatchBranch) -> Interval<usize> {
+        Self::statements_size(&match_branch.statements)
+    }
+    fn statements_size(statements: &Vec<IntermediateStatement>) -> Interval<usize> {
+        statements
+            .iter()
+            .map(Self::statement_size)
+            .fold(Interval::singleton(0), Interval::add)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashSet;
+    use std::{cmp::min, collections::HashSet};
 
     use super::*;
 
+    use interval::Interval;
+    use itertools::Itertools;
     use lowering::{
-        AtomicTypeEnum, Boolean, BuiltInFn, Id, Integer, IntermediateArg, IntermediateCtorCall,
-        IntermediateElementAccess, IntermediateFnCall, IntermediateFnType, IntermediateLambda,
-        IntermediateMemory, IntermediateTupleExpression, IntermediateTupleType,
-        IntermediateUnionType, Location, DEFAULT_CONTEXT,
+        AtomicTypeEnum, Boolean, BuiltInFn, Id, Integer, IntermediateArg, IntermediateAssignment,
+        IntermediateCtorCall, IntermediateElementAccess, IntermediateFnCall, IntermediateFnType,
+        IntermediateIfStatement, IntermediateLambda, IntermediateMatchBranch,
+        IntermediateMatchStatement, IntermediateMemory, IntermediateStatement,
+        IntermediateTupleExpression, IntermediateTupleType, IntermediateUnionType, Location,
+        DEFAULT_CONTEXT,
     };
     use test_case::test_case;
 
@@ -137,6 +199,9 @@ mod tests {
     const FCS: Lazy<usize> = Lazy::new(|| CODE_SIZE_CONSTANTS.fn_call_size);
     const CCS: Lazy<usize> = Lazy::new(|| CODE_SIZE_CONSTANTS.ctor_call_size);
     const LS: Lazy<usize> = Lazy::new(|| CODE_SIZE_CONSTANTS.lambda_size);
+    const AS: Lazy<usize> = Lazy::new(|| CODE_SIZE_CONSTANTS.assignment_size);
+    const ISS: Lazy<usize> = Lazy::new(|| CODE_SIZE_CONSTANTS.if_statement_size);
+    const MSS: Lazy<usize> = Lazy::new(|| CODE_SIZE_CONSTANTS.match_statement_size);
 
     #[test]
     fn exhaustive_operator_test() {
@@ -337,6 +402,163 @@ mod tests {
     )]
     fn test_expression_size(expression: IntermediateExpression, expected_size: usize) {
         let size = CodeSizeEstimator::expression_size(&expression);
+        assert_eq!(size, expected_size)
+    }
+
+    #[test_case(
+        {
+            let expression = IntermediateCtorCall{
+                idx: 0,
+                data: Some(Boolean{value: false}.into()),
+                type_: IntermediateUnionType(
+                    vec![
+                        Some(AtomicTypeEnum::INT.into()),
+                        Some(AtomicTypeEnum::BOOL.into())
+                    ]
+                )
+            }.into();
+            let statement_size = *AS + CodeSizeEstimator::expression_size(&expression);
+            (
+                IntermediateAssignment{
+                    expression: expression,
+                    location: Location::new()
+                }.into(),
+                Interval::new(statement_size, statement_size)
+            )
+        };
+        "assignment"
+    )]
+    #[test_case(
+        {
+            let args = vec![
+                IntermediateArg{
+                    location: Location::new(),
+                    type_: AtomicTypeEnum::INT.into()
+                },
+                IntermediateArg{
+                    location: Location::new(),
+                    type_: AtomicTypeEnum::INT.into()
+                },
+            ];
+            let target = Location::new();
+            let small_assignment = IntermediateAssignment{
+                location: target.clone(),
+                expression: IntermediateValue::from(args[0].clone()).into()
+            };
+            let large_assignment = IntermediateAssignment{
+                expression: IntermediateFnCall{
+                    fn_: BuiltInFn(
+                        Id::from("*"),
+                        IntermediateFnType(
+                            vec![AtomicTypeEnum::INT.into(), AtomicTypeEnum::INT.into()],
+                            Box::new(AtomicTypeEnum::INT.into())
+                        ).into()
+                    ).into(),
+                    args: args.into_iter().map(IntermediateValue::from).collect_vec()
+                }.into(),
+                location: Location::new()
+            };
+            let large_final_assignment = IntermediateAssignment{
+                expression: IntermediateValue::from(large_assignment.clone()).into(),
+                location: target
+            };
+            let condition = IntermediateMemory{
+                location: Location::new(),
+                type_: AtomicTypeEnum::BOOL.into()
+            };
+            let small_statement_size = CodeSizeEstimator::statement_size(&small_assignment.clone().into()).lower();
+            let large_statements_size = CodeSizeEstimator::statement_size(&large_assignment.clone().into()).lower() + CodeSizeEstimator::statement_size(&large_final_assignment.clone().into()).lower();
+            let condition_size = CodeSizeEstimator::value_size(&condition.clone().into());
+            let (lower_bound, upper_bound) = (small_statement_size + condition_size + *ISS, large_statements_size + condition_size + *ISS);
+            (
+                IntermediateIfStatement{
+                    condition: condition.into(),
+                    branches: (
+                        vec![small_assignment.into()],
+                        vec![
+                            large_assignment.into(),
+                            large_final_assignment.into()
+                        ],
+                    )
+                }.into(),
+                Interval::new(lower_bound, upper_bound)
+            )
+        };
+        "if statement"
+    )]
+    #[test_case(
+        {
+            let medium_arg = IntermediateArg{
+                location: Location::new(),
+                type_: AtomicTypeEnum::INT.into()
+            };
+            let large_arg = IntermediateArg{
+                location: Location::new(),
+                type_: AtomicTypeEnum::INT.into()
+            };
+            let target = Location::new();
+            let small_assignment = IntermediateAssignment{
+                location: target.clone(),
+                expression: IntermediateValue::from(Integer{value: 4}).into()
+            };
+            let medium_assignment = IntermediateAssignment{
+                expression: medium_arg.clone().into(),
+                location: target.clone()
+            };
+            let large_assignment = IntermediateAssignment{
+                expression: IntermediateFnCall{
+                    fn_: BuiltInFn(
+                        Id::from("*"),
+                        IntermediateFnType(
+                            vec![AtomicTypeEnum::INT.into(), AtomicTypeEnum::INT.into()],
+                            Box::new(AtomicTypeEnum::INT.into())
+                        ).into()
+                    ).into(),
+                    args: vec![
+                        Integer{value: 9}.into(),
+                        large_arg.clone().into()
+                    ]
+                }.into(),
+                location: target
+            };
+            let subject = IntermediateMemory{
+                location: Location::new(),
+                type_: IntermediateUnionType(
+                    vec![Some(AtomicTypeEnum::INT.into()), None, Some(AtomicTypeEnum::INT.into())]
+                ).into()
+            };
+            let small_statement_size = CodeSizeEstimator::statement_size(&small_assignment.clone().into()).lower();
+            let medium_statement_size = CodeSizeEstimator::statement_size(&medium_assignment.clone().into()).lower();
+            let large_statement_size = CodeSizeEstimator::statement_size(&large_assignment.clone().into()).lower();
+            let subject_size = CodeSizeEstimator::value_size(&subject.clone().into());
+            dbg!(subject_size, *MSS);
+            let (lower_bound, upper_bound) = (min(small_statement_size, medium_statement_size) + subject_size + *MSS, large_statement_size + subject_size + *MSS);
+            (
+                IntermediateMatchStatement{
+                    subject: subject.into(),
+                    branches: vec![
+                        IntermediateMatchBranch{
+                            target: None,
+                            statements: vec![small_assignment.into()],
+                        },
+                        IntermediateMatchBranch{
+                            target: Some(medium_arg),
+                            statements: vec![medium_assignment.into()],
+                        },
+                        IntermediateMatchBranch{
+                            target: Some(large_arg),
+                            statements: vec![large_assignment.into()],
+                        },
+                    ]
+                }.into(),
+                Interval::new(lower_bound, upper_bound)
+            )
+        };
+        "match statement"
+    )]
+    fn test_statement_size(statement_size: (IntermediateStatement, Interval<usize>)) {
+        let (statement, expected_size) = statement_size;
+        let size = CodeSizeEstimator::statement_size(&statement);
         assert_eq!(size, expected_size)
     }
 }
