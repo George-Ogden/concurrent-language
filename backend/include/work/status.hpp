68878fd10730fddd3a7d8f02a4359bcac18a253f
George Ogden <george-ogden@outlook.com>
2025-03-17 15:30:08 +0000

    rewrite status transitions

--- backend/include/work/status.hpp
+++ backend/include/work/status.hpp
@@ -6,20 +6,44 @@
 #include <memory>
 
 class Status {
-    static inline constexpr std::size_t OWNED_IDX = 0;
-    static inline constexpr std::size_t OWNED_WIDTH = 1;
-    static inline constexpr std::size_t PRIORITY_IDX = 1;
-    static inline constexpr std::size_t PRIORITY_WIDTH = 1;
-    static inline constexpr std::size_t DONE_IDX = 2;
-    static inline constexpr std::size_t DONE_WIDTH = 1;
-    AtomicSharedEnum<OWNED_WIDTH, PRIORITY_WIDTH, DONE_WIDTH> value;
+    enum WorkStatus { UNAVAILABLE, AVAILABLE, FULL, MAX };
+    static inline constexpr std::size_t WORK_IDX = 0;
+    static inline constexpr std::size_t WORK_WIDTH = 2;
+    static_assert(MAX <= (1ULL << WORK_WIDTH));
+    static inline constexpr std::size_t QUEUE_IDX = 1;
+    static inline constexpr std::size_t QUEUE_WIDTH = 1;
+    AtomicSharedEnum<WORK_WIDTH, QUEUE_WIDTH> value;
 
   public:
-    Status();
-    bool acquire();
-    bool release();
-    bool prioritize();
-    bool priority() const;
-    void finish();
-    bool done() const;
+    Status() = default;
+    bool queued() const { return value.load<QUEUE_IDX>(); }
+    bool available() const { return value.load<WORK_IDX>() == AVAILABLE; }
+    bool unavailable() const { return value.load<WORK_IDX>() == UNAVAILABLE; }
+    bool full() const { return value.load<WORK_IDX>() == FULL; }
+
+    bool request() {
+        return value.compare_exchange<WORK_IDX>(UNAVAILABLE, AVAILABLE);
+    }
+    bool cancel() {
+        return value.compare_exchange<WORK_IDX>(AVAILABLE, UNAVAILABLE);
+    }
+    bool fill() {
+        if (value.compare_exchange<WORK_IDX, WORK_IDX>(AVAILABLE, FULL)) {
+            value.store<QUEUE_IDX>(false);
+            return true;
+        } else {
+            value.store<QUEUE_IDX>(false);
+            return false;
+        }
+    }
+    bool complete() {
+        return value.compare_exchange<WORK_IDX>(FULL, UNAVAILABLE);
+    }
+    bool enqueue() {
+        return value.compare_exchange<WORK_IDX, QUEUE_IDX, QUEUE_IDX>(
+            AVAILABLE, false, true);
+    }
+    bool dequeue() {
+        return value.compare_exchange<WORK_IDX, QUEUE_IDX>(UNAVAILABLE, false);
+    }
 };
--- backend/test/test_status.hpp
+++ backend/test/test_status.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "system/thread_manager.tpp"
-#include "work/status.tpp"
+#include "work/status.hpp"
 
 #include <gtest/gtest.h>
 
@@ -14,48 +14,120 @@ class StatusTransitionTest : public ::testing::Test {
     void TearDown() override { ThreadManager::reset_concurrency_override(); }
 };
 
-TEST_F(StatusTransitionTest, AcquireRelease) {
+TEST_F(StatusTransitionTest, FillJob) {
     Status status;
-    ASSERT_FALSE(status.done());
-    ASSERT_TRUE(status.acquire());
-    ASSERT_TRUE(status.release());
-    ASSERT_TRUE(status.acquire());
-    ASSERT_FALSE(status.acquire());
-    ASSERT_TRUE(status.release());
-    ASSERT_FALSE(status.done());
+    ASSERT_FALSE(status.queued());
+    ASSERT_FALSE(status.available());
+    ASSERT_TRUE(status.unavailable());
+    ASSERT_FALSE(status.full());
+
+    ASSERT_TRUE(status.request());
+
+    ASSERT_FALSE(status.queued());
+    ASSERT_TRUE(status.available());
+    ASSERT_FALSE(status.unavailable());
+    ASSERT_FALSE(status.full());
+
+    ASSERT_TRUE(status.enqueue());
+
+    ASSERT_TRUE(status.queued());
+    ASSERT_TRUE(status.available());
+    ASSERT_FALSE(status.unavailable());
+    ASSERT_FALSE(status.full());
+
+    ASSERT_TRUE(status.fill());
+
+    ASSERT_FALSE(status.queued());
+    ASSERT_FALSE(status.available());
+    ASSERT_FALSE(status.unavailable());
+    ASSERT_TRUE(status.full());
+
+    ASSERT_FALSE(status.fill());
+    ASSERT_TRUE(status.complete());
+
+    ASSERT_FALSE(status.queued());
+    ASSERT_FALSE(status.available());
+    ASSERT_TRUE(status.unavailable());
+    ASSERT_FALSE(status.full());
 }
 
-TEST_F(StatusTransitionTest, RequiredUnheld) {
+TEST_F(StatusTransitionTest, CancelJob) {
     Status status;
-    ASSERT_FALSE(status.done());
-    ASSERT_FALSE(status.priority());
-    ASSERT_TRUE(status.prioritize());
-    ASSERT_TRUE(status.priority());
-    ASSERT_FALSE(status.prioritize());
-    ASSERT_TRUE(status.priority());
-    ASSERT_TRUE(status.acquire());
-    ASSERT_FALSE(status.release());
-    ASSERT_FALSE(status.done());
+    ASSERT_FALSE(status.queued());
+    ASSERT_FALSE(status.available());
+    ASSERT_TRUE(status.unavailable());
+    ASSERT_FALSE(status.full());
+
+    ASSERT_TRUE(status.request());
+
+    ASSERT_FALSE(status.queued());
+    ASSERT_TRUE(status.available());
+    ASSERT_FALSE(status.unavailable());
+    ASSERT_FALSE(status.full());
+
+    ASSERT_TRUE(status.enqueue());
+
+    ASSERT_TRUE(status.queued());
+    ASSERT_TRUE(status.available());
+    ASSERT_FALSE(status.unavailable());
+    ASSERT_FALSE(status.full());
+
+    ASSERT_TRUE(status.cancel());
+
+    ASSERT_TRUE(status.queued());
+    ASSERT_FALSE(status.available());
+    ASSERT_TRUE(status.unavailable());
+
+    ASSERT_FALSE(status.fill());
+    ASSERT_TRUE(status.request());
+
+    ASSERT_FALSE(status.queued());
+    ASSERT_TRUE(status.available());
+    ASSERT_FALSE(status.unavailable());
+    ASSERT_FALSE(status.full());
+
+    ASSERT_TRUE(status.fill());
+    ASSERT_FALSE(status.cancel());
+
+    ASSERT_FALSE(status.queued());
+    ASSERT_FALSE(status.available());
+    ASSERT_FALSE(status.unavailable());
+    ASSERT_TRUE(status.full());
 }
 
-TEST_F(StatusTransitionTest, RequiredHeld) {
+TEST_F(StatusTransitionTest, Dequeue) {
     Status status;
-    ASSERT_FALSE(status.done());
-    ASSERT_TRUE(status.acquire());
-    ASSERT_FALSE(status.priority());
-    ASSERT_TRUE(status.prioritize());
-    ASSERT_TRUE(status.priority());
-    ASSERT_FALSE(status.prioritize());
-    ASSERT_TRUE(status.priority());
-    ASSERT_FALSE(status.release());
-    ASSERT_FALSE(status.done());
-}
+    ASSERT_FALSE(status.queued());
+    ASSERT_FALSE(status.available());
+    ASSERT_TRUE(status.unavailable());
+    ASSERT_FALSE(status.full());
 
-TEST_F(StatusTransitionTest, Finish) {
-    Status status;
-    ASSERT_FALSE(status.done());
-    ASSERT_TRUE(status.acquire());
-    ASSERT_FALSE(status.done());
-    status.finish();
-    ASSERT_TRUE(status.done());
+    ASSERT_TRUE(status.request());
+
+    ASSERT_FALSE(status.queued());
+    ASSERT_TRUE(status.available());
+    ASSERT_FALSE(status.unavailable());
+    ASSERT_FALSE(status.full());
+
+    ASSERT_TRUE(status.enqueue());
+
+    ASSERT_TRUE(status.queued());
+    ASSERT_TRUE(status.available());
+    ASSERT_FALSE(status.unavailable());
+    ASSERT_FALSE(status.full());
+
+    ASSERT_FALSE(status.enqueue());
+    ASSERT_FALSE(status.dequeue());
+    ASSERT_TRUE(status.cancel());
+
+    ASSERT_TRUE(status.queued());
+    ASSERT_FALSE(status.available());
+    ASSERT_TRUE(status.unavailable());
+
+    ASSERT_TRUE(status.dequeue());
+
+    ASSERT_FALSE(status.queued());
+    ASSERT_FALSE(status.available());
+    ASSERT_TRUE(status.unavailable());
+    ASSERT_FALSE(status.full());
 }
