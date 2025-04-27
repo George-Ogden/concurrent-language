b47d82f2d5be9ceb149c46200937b0d196c9efbe
George Ogden <george-ogden@outlook.com>
2025-04-27 14:40:54 +0100

    batch statements for reordering

--- translation/src/statement_reorderer.rs
+++ translation/src/statement_reorderer.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashMap, HashSet};
 
+use itertools::Either::{self, Left, Right};
 use itertools::Itertools;
 
 use crate::{Assignment, Await, Expression, FnCall, Memory, Statement, Value};
@@ -56,6 +57,59 @@ impl StatementReorderer {
         }
     }
 
+    /// Batch statements into blocks that can be independently reordered.
+    fn batch_statements(
+        &self,
+        statements: Vec<Statement>,
+    ) -> impl Iterator<Item = Either<Statement, Vec<Statement>>> {
+        // Store the next batch.
+        let mut next_batch: Option<Either<Statement, Vec<Statement>>> = None;
+        statements.into_iter().batching(move |it| {
+            // Iterate through the vector, returning the next complete batch.
+            loop {
+                match it.next() {
+                    Some(statement) => match &statement {
+                        Statement::Await(_) | Statement::Assignment(_)
+                            if !matches!(
+                                &statement,
+                                Statement::Assignment(Assignment {
+                                    target: _,
+                                    value: Expression::ClosureInstantiation(_)
+                                })
+                            ) =>
+                        {
+                            let batch;
+                            (batch, next_batch) = match next_batch.clone() {
+                                Some(Left(stmt)) => {
+                                    (Some(Left(stmt)), Some(Right(vec![statement])))
+                                }
+                                Some(Right(mut statements)) => {
+                                    statements.push(statement);
+                                    (None, Some(Right(statements)))
+                                }
+                                None => (None, Some(Right(vec![statement]))),
+                            };
+                            if batch.is_some() {
+                                return batch;
+                            }
+                        }
+                        _ => {
+                            let batch = next_batch.clone();
+                            next_batch = Some(Left(statement));
+                            if batch.is_some() {
+                                return batch;
+                            }
+                        }
+                    },
+                    None => {
+                        let batch = next_batch.clone();
+                        next_batch = None;
+                        return batch;
+                    }
+                }
+            }
+        })
+    }
     /// Construct dependency graph on subset of statements.
     fn construct_graph(&self, statements: Vec<Statement>) -> Graph {
         let mut graph = Graph::new();
@@ -101,12 +155,13 @@ impl StatementReorderer {
 #[cfg(test)]
 mod tests {
     use crate::{
-        Assignment, Await, BuiltIn, Declaration, FnCall, FnType, Id, IfStatement, MatchBranch,
-        MatchStatement, Name, Statement, UnionType,
+        Assignment, Await, BuiltIn, ClosureInstantiation, Declaration, FnCall, FnType, Id,
+        IfStatement, MatchBranch, MatchStatement, Name, Statement, TupleExpression, UnionType,
     };
 
     use super::*;
 
+    use itertools::Either;
     use lowering::{AtomicTypeEnum, Boolean};
     use test_case::test_case;
 
@@ -445,5 +500,315 @@ mod tests {
         reorderer.fn_calls = fn_calls;
         let graph = reorderer.construct_graph(statements);
         assert_eq!(expected_graph, graph);
+    }
+
+    #[test_case(
+        vec![
+            Await(vec![Memory(Id::from("f"))]).into(),
+            Assignment{
+                target: Memory(Id::from("t1")),
+                value: FnCall {
+                    fn_: Memory(Id::from("f")).into(),
+                    fn_type: FnType(
+                        vec![AtomicTypeEnum::INT.into()],
+                        Box::new(AtomicTypeEnum::BOOL.into())
+                    ),
+                    args: vec![
+                        Memory(Id::from("m1")).into(),
+                    ]
+                }.into(),
+            }.into(),
+            Await(vec![Memory(Id::from("f"))]).into(),
+            Assignment{
+                target: Memory(Id::from("t2")),
+                value: FnCall {
+                    fn_: Memory(Id::from("f")).into(),
+                    fn_type: FnType(
+                        vec![AtomicTypeEnum::INT.into()],
+                        Box::new(AtomicTypeEnum::BOOL.into())
+                    ),
+                    args: vec![
+                        Memory(Id::from("m2")).into(),
+                    ]
+                }.into(),
+            }.into(),
+        ],
+        vec![Right(vec![
+            Await(vec![Memory(Id::from("f"))]).into(),
+            Assignment{
+                target: Memory(Id::from("t1")),
+                value: FnCall {
+                    fn_: Memory(Id::from("f")).into(),
+                    fn_type: FnType(
+                        vec![AtomicTypeEnum::INT.into()],
+                        Box::new(AtomicTypeEnum::BOOL.into())
+                    ),
+                    args: vec![
+                        Memory(Id::from("m1")).into(),
+                    ]
+                }.into(),
+            }.into(),
+            Await(vec![Memory(Id::from("f"))]).into(),
+            Assignment{
+                target: Memory(Id::from("t2")),
+                value: FnCall {
+                    fn_: Memory(Id::from("f")).into(),
+                    fn_type: FnType(
+                        vec![AtomicTypeEnum::INT.into()],
+                        Box::new(AtomicTypeEnum::BOOL.into())
+                    ),
+                    args: vec![
+                        Memory(Id::from("m2")).into(),
+                    ]
+                }.into(),
+            }.into(),
+        ])];
+        "flat batches"
+    )]
+    #[test_case(
+        vec![
+            Await(vec![Memory(Id::from("f"))]).into(),
+            Assignment{
+                target: Memory(Id::from("t1")),
+                value: FnCall {
+                    fn_: Memory(Id::from("f")).into(),
+                    fn_type: FnType(
+                        vec![AtomicTypeEnum::INT.into()],
+                        Box::new(AtomicTypeEnum::BOOL.into())
+                    ),
+                    args: vec![
+                        Memory(Id::from("x0")).into(),
+                    ]
+                }.into(),
+            }.into(),
+            Declaration{
+                memory: Memory(Id::from("shared")),
+                type_: AtomicTypeEnum::BOOL.into()
+            }.into(),
+            Await(vec![Memory(Id::from("condition"))]).into(),
+            IfStatement{
+                condition: Memory(Id::from("condition")).into(),
+                branches: (
+                    Vec::new(),
+                    Vec::new(),
+                )
+            }.into(),
+        ],
+        vec![
+            Right(vec![
+                Await(vec![Memory(Id::from("f"))]).into(),
+                Assignment{
+                    target: Memory(Id::from("t1")),
+                    value: FnCall {
+                        fn_: Memory(Id::from("f")).into(),
+                        fn_type: FnType(
+                            vec![AtomicTypeEnum::INT.into()],
+                            Box::new(AtomicTypeEnum::BOOL.into())
+                        ),
+                        args: vec![
+                            Memory(Id::from("x0")).into(),
+                        ]
+                    }.into(),
+                }.into(),
+            ]),
+            Left(
+                Declaration{
+                    memory: Memory(Id::from("shared")),
+                    type_: AtomicTypeEnum::BOOL.into()
+                }.into(),
+            ),
+            Right(vec![
+                Await(vec![Memory(Id::from("condition"))]).into(),
+            ]),
+            Left(IfStatement{
+                condition: Memory(Id::from("condition")).into(),
+                branches: (
+                    Vec::new(),
+                    Vec::new(),
+                )
+            }.into()),
+        ];
+        "if statement batches"
+    )]
+    #[test_case(
+        vec![
+            Declaration{
+                memory: Memory(Id::from("shared")),
+                type_: AtomicTypeEnum::BOOL.into()
+            }.into(),
+            MatchStatement{
+                expression: (
+                    Memory(Id::from("subject")).into(),
+                    UnionType(vec![Name::from("A"), Name::from("B"), Name::from("C")])
+                ),
+                auxiliary_memory: Memory(Id::from("aux")),
+                branches: vec![
+                    MatchBranch {
+                        target: Some(Memory(Id::from("a"))),
+                        statements: Vec::new(),
+                    },
+                    MatchBranch {
+                        target: Some(Memory(Id::from("b"))),
+                        statements: Vec::new(),
+                    },
+                    MatchBranch {
+                        target: Some(Memory(Id::from("c"))),
+                        statements: Vec::new(),
+                    },
+                ]
+            }.into(),
+            Await(vec![Memory(Id::from("f"))]).into(),
+            Assignment{
+                target: Memory(Id::from("y")),
+                value: FnCall {
+                    fn_: Memory(Id::from("f")).into(),
+                    fn_type: FnType(
+                        vec![AtomicTypeEnum::INT.into()],
+                        Box::new(AtomicTypeEnum::BOOL.into())
+                    ),
+                    args: vec![
+                        Memory(Id::from("x")).into(),
+                    ]
+                }.into(),
+            }.into(),
+        ],
+        vec![
+            Left(Declaration{
+                memory: Memory(Id::from("shared")),
+                type_: AtomicTypeEnum::BOOL.into()
+            }.into()),
+            Left(MatchStatement{
+                expression: (
+                    Memory(Id::from("subject")).into(),
+                    UnionType(vec![Name::from("A"), Name::from("B"), Name::from("C")])
+                ),
+                auxiliary_memory: Memory(Id::from("aux")),
+                branches: vec![
+                    MatchBranch {
+                        target: Some(Memory(Id::from("a"))),
+                        statements: Vec::new(),
+                    },
+                    MatchBranch {
+                        target: Some(Memory(Id::from("b"))),
+                        statements: Vec::new(),
+                    },
+                    MatchBranch {
+                        target: Some(Memory(Id::from("c"))),
+                        statements: Vec::new(),
+                    },
+                ]
+            }.into()),
+            Right(vec![
+                Await(vec![Memory(Id::from("f"))]).into(),
+                Assignment{
+                    target: Memory(Id::from("y")),
+                    value: FnCall {
+                        fn_: Memory(Id::from("f")).into(),
+                        fn_type: FnType(
+                            vec![AtomicTypeEnum::INT.into()],
+                            Box::new(AtomicTypeEnum::BOOL.into())
+                        ),
+                        args: vec![
+                            Memory(Id::from("x")).into(),
+                        ]
+                    }.into(),
+                }.into(),
+            ])
+        ];
+        "match statement batches"
+    )]
+    #[test_case(
+        vec![
+            Declaration {
+                type_: FnType(
+                    vec![AtomicTypeEnum::INT.into()],
+                    Box::new(AtomicTypeEnum::INT.into())
+                ).into(),
+                memory: Memory(Id::from("m1"))
+            }.into(),
+            Assignment {
+                target: Memory(Id::from("m1")),
+                value: ClosureInstantiation { name: Name::from("F0"), env: None }.into(),
+            }.into(),
+            Assignment {
+                target: Memory(Id::from("m4")),
+                value: TupleExpression(vec![Memory(Id::from("m1")).into()]).into(),
+            }.into(),
+            Declaration {
+                type_: FnType(Vec::new(), Box::new(AtomicTypeEnum::INT.into())).into(),
+                memory: Memory(Id::from("m5"))
+            }.into(),
+            Assignment {
+                target: Memory(Id::from("m5")),
+                value: ClosureInstantiation {
+                    name: Name::from("F1"),
+                    env: Some(Memory(Id::from("m4")).into())
+                }.into(),
+            }.into(),
+            Await(vec![Memory(Id::from("m5"))]).into(),
+            Assignment {
+                target: Memory(Id::from("m6")),
+                value: FnCall {
+                    fn_: Memory(Id::from("m5")).into(),
+                    fn_type: FnType(Vec::new(), Box::new(AtomicTypeEnum::INT.into())).into(),
+                    args: Vec::new()
+                }.into(),
+            }.into()
+        ],
+        vec![
+            Left(Declaration {
+                type_: FnType(
+                    vec![AtomicTypeEnum::INT.into()],
+                    Box::new(AtomicTypeEnum::INT.into())
+                ).into(),
+                memory: Memory(Id::from("m1"))
+            }.into()),
+            Left(Assignment {
+                target: Memory(Id::from("m1")),
+                value: ClosureInstantiation { name: Name::from("F0"), env: None }.into(),
+            }.into()),
+            Right(vec![
+                Assignment {
+                    target: Memory(Id::from("m4")),
+                    value: TupleExpression(vec![Memory(Id::from("m1")).into()]).into(),
+                }.into()
+            ]),
+            Left(Declaration {
+                type_: FnType(Vec::new(), Box::new(AtomicTypeEnum::INT.into())).into(),
+                memory: Memory(Id::from("m5"))
+            }.into()),
+            Left(Assignment {
+                target: Memory(Id::from("m5")),
+                value: ClosureInstantiation {
+                    name: Name::from("F1"),
+                    env: Some(Memory(Id::from("m4")).into())
+                }.into(),
+            }.into()),
+            Right(vec![
+                Await(vec![Memory(Id::from("m5"))]).into(),
+                Assignment {
+                    target: Memory(Id::from("m6")),
+                    value: FnCall {
+                        fn_: Memory(Id::from("m5")).into(),
+                        fn_type: FnType(Vec::new(), Box::new(AtomicTypeEnum::INT.into())).into(),
+                        args: Vec::new()
+                    }.into(),
+                }.into()
+            ])
+        ];
+        "closure instantiation"
+    )]
+    fn test_batch_statements(
+        statements: Vec<Statement>,
+        expected_batches: Vec<Either<Statement, Vec<Statement>>>,
+    ) {
+        let reorderer = StatementReorderer::new();
+        let batches = reorderer.batch_statements(statements).collect_vec();
+        assert_eq!(expected_batches, batches);
+        for batch in batches {
+            if let Right(statements) = batch {
+                reorderer.construct_graph(statements);
+            }
+        }
     }
 }
