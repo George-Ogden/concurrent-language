fe80106133a13c7fcd26625d1e64b6a594d9d53a
George Ogden <george-ogden@outlook.com>
2025-04-28 12:08:20 +0100

    enqueue match statements

--- translation/src/enqueuer.rs
+++ translation/src/enqueuer.rs
@@ -2,7 +2,9 @@ use std::collections::HashSet;
 
 use itertools::Itertools;
 
-use crate::{Assignment, Await, Enqueue, IfStatement, Memory, Statement};
+use crate::{
+    Assignment, Await, Enqueue, IfStatement, MatchBranch, MatchStatement, Memory, Statement,
+};
 
 struct Enqueuer {}
 
@@ -54,7 +56,48 @@ impl Enqueuer {
                     }
                     .into()]
                 }
-                Statement::MatchStatement(match_) => todo!(),
+                Statement::MatchStatement(MatchStatement {
+                    expression,
+                    branches,
+                    auxiliary_memory,
+                }) => {
+                    let ((statements, all_required), targets): ((Vec<_>, Vec<_>), Vec<_>) =
+                        branches
+                            .into_iter()
+                            .map(|MatchBranch { statements, target }| {
+                                (self.enqueue_statements(statements), target)
+                            })
+                            .unzip();
+                    let mut intersection = None;
+                    for required in &all_required {
+                        intersection = match intersection {
+                            None => Some(required.clone()),
+                            Some(intersection) => {
+                                Some(intersection.intersection(required).cloned().collect())
+                            }
+                        }
+                    }
+                    let intersection = intersection.unwrap();
+                    let branches = statements
+                        .into_iter()
+                        .zip_eq(all_required.into_iter())
+                        .zip_eq(targets.into_iter())
+                        .map(|((mut statements, required), target)| {
+                            let difference = required.difference(&intersection).cloned();
+                            let enqueues = difference.map(|memory| Enqueue(memory).into());
+                            statements.extend(enqueues);
+                            statements.reverse();
+                            MatchBranch { target, statements }
+                        })
+                        .collect_vec();
+                    required.extend(intersection);
+                    vec![MatchStatement {
+                        expression,
+                        branches,
+                        auxiliary_memory,
+                    }
+                    .into()]
+                }
                 statement => vec![statement],
             })
             .collect_vec();
@@ -262,6 +305,141 @@ mod tests {
         ],
         vec!["c"];
         "if branches no overlap"
+    )]
+    #[test_case(
+        vec![
+            Await(vec![Memory(Id::from("s"))]).into(),
+            MatchStatement {
+                expression: (Memory(Id::from("s")).into(), UnionType(vec![Name::from("wrapper")])),
+                auxiliary_memory: Memory(Id::from("aux")).into(),
+                branches: vec![
+                    MatchBranch {
+                        target: Some(Memory(Id::from("x"))),
+                        statements: vec![
+                            Await(vec![Memory(Id::from("f"))]).into(),
+                            Assignment{
+                                target: Memory(Id::from("y")),
+                                value: FnCall {
+                                    fn_: Memory(Id::from("f")).into(),
+                                    fn_type: FnType(
+                                        vec![AtomicTypeEnum::INT.into()],
+                                        Box::new(AtomicTypeEnum::BOOL.into())
+                                    ),
+                                    args: vec![
+                                        Memory(Id::from("x")).into(),
+                                    ]
+                                }.into(),
+                            }.into(),
+                        ]
+                    }
+                ]
+            }.into()
+        ],
+        vec![
+            Await(vec![Memory(Id::from("s"))]).into(),
+            MatchStatement {
+                expression: (Memory(Id::from("s")).into(), UnionType(vec![Name::from("wrapper")])),
+                auxiliary_memory: Memory(Id::from("aux")).into(),
+                branches: vec![
+                    MatchBranch {
+                        target: Some(Memory(Id::from("x"))),
+                        statements: vec![
+                            Await(vec![Memory(Id::from("f"))]).into(),
+                            Assignment{
+                                target: Memory(Id::from("y")),
+                                value: FnCall {
+                                    fn_: Memory(Id::from("f")).into(),
+                                    fn_type: FnType(
+                                        vec![AtomicTypeEnum::INT.into()],
+                                        Box::new(AtomicTypeEnum::BOOL.into())
+                                    ),
+                                    args: vec![
+                                        Memory(Id::from("x")).into(),
+                                    ]
+                                }.into(),
+                            }.into(),
+                        ]
+                    }
+                ]
+            }.into()
+        ],
+        vec!["f","s"];
+        "match single branch"
+    )]
+    #[test_case(
+        vec![
+            Await(vec![Memory(Id::from("s"))]).into(),
+            MatchStatement {
+                expression: (Memory(Id::from("s")).into(), UnionType(vec![Name::from("wrapper"), Name::from("two"), Name::from("three")])),
+                auxiliary_memory: Memory(Id::from("aux")).into(),
+                branches: vec![
+                    MatchBranch {
+                        target: Some(Memory(Id::from("x"))),
+                        statements: vec![
+                            Await(vec![Memory(Id::from("f"))]).into(),
+                            Assignment{
+                                target: Memory(Id::from("y")),
+                                value: FnCall {
+                                    fn_: Memory(Id::from("f")).into(),
+                                    fn_type: FnType(
+                                        vec![AtomicTypeEnum::INT.into()],
+                                        Box::new(AtomicTypeEnum::BOOL.into())
+                                    ),
+                                    args: vec![
+                                        Memory(Id::from("x")).into(),
+                                    ]
+                                }.into(),
+                            }.into(),
+                        ]
+                    },
+                    MatchBranch {
+                        target: Some(Memory(Id::from("j"))),
+                        statements: vec![
+                            Await(vec![Memory(Id::from("f"))]).into(),
+                            Await(vec![Memory(Id::from("j"))]).into(),
+                        ]
+                    }
+                ]
+            }.into()
+        ],
+        vec![
+            Await(vec![Memory(Id::from("s"))]).into(),
+            MatchStatement {
+                expression: (Memory(Id::from("s")).into(), UnionType(vec![Name::from("wrapper"), Name::from("two"), Name::from("three")])),
+                auxiliary_memory: Memory(Id::from("aux")).into(),
+                branches: vec![
+                    MatchBranch {
+                        target: Some(Memory(Id::from("x"))),
+                        statements: vec![
+                            Await(vec![Memory(Id::from("f"))]).into(),
+                            Assignment{
+                                target: Memory(Id::from("y")),
+                                value: FnCall {
+                                    fn_: Memory(Id::from("f")).into(),
+                                    fn_type: FnType(
+                                        vec![AtomicTypeEnum::INT.into()],
+                                        Box::new(AtomicTypeEnum::BOOL.into())
+                                    ),
+                                    args: vec![
+                                        Memory(Id::from("x")).into(),
+                                    ]
+                                }.into(),
+                            }.into(),
+                        ]
+                    },
+                    MatchBranch {
+                        target: Some(Memory(Id::from("j"))),
+                        statements: vec![
+                            Enqueue(Memory(Id::from("j"))).into(),
+                            Await(vec![Memory(Id::from("f"))]).into(),
+                            Await(vec![Memory(Id::from("j"))]).into(),
+                        ]
+                    }
+                ]
+            }.into()
+        ],
+        vec!["f","s"];
+        "match multiple branches"
     )]
     fn test_enqueue_statements(
         statements: Vec<Statement>,
