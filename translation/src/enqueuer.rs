c6c6ddff588d81c03eb6192ef78187f2a140d37a
George Ogden <george-ogden@outlook.com>
2025-04-28 11:57:46 +0100

    enqueue if statements

--- translation/src/enqueuer.rs
+++ translation/src/enqueuer.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 use itertools::Itertools;
 
-use crate::{Assignment, Await, Enqueue, Memory, Statement};
+use crate::{Assignment, Await, Enqueue, IfStatement, Memory, Statement};
 
 struct Enqueuer {}
 
@@ -10,36 +10,61 @@ impl Enqueuer {
     fn new() -> Self {
         Self {}
     }
-    fn enqueue(&self, statements: Vec<Statement>) -> (Vec<Statement>, HashSet<Memory>) {
+    /// Add enqueue instructions to statements, returning the updated statements in _reverse_ order.
+    fn enqueue_statements(&self, statements: Vec<Statement>) -> (Vec<Statement>, HashSet<Memory>) {
         let mut required = HashSet::new();
-        let mut statements = statements
+        let statements = statements
             .into_iter()
             .rev()
-            .flat_map(|statement| match &statement {
-                Statement::Await(Await(memory)) => {
+            .flat_map(|statement| match statement {
+                Statement::Await(Await(ref memory)) => {
                     required.extend(memory.clone());
                     vec![statement]
                 }
-                Statement::Assignment(Assignment { target, value: _ }) => {
+                Statement::Assignment(Assignment {
+                    ref target,
+                    value: _,
+                }) => {
                     if required.remove(target) {
                         vec![Enqueue(target.clone()).into(), statement]
                     } else {
                         vec![statement]
                     }
                 }
-                Statement::IfStatement(if_) => todo!(),
+                Statement::IfStatement(IfStatement {
+                    condition,
+                    branches: (true_branch, false_branch),
+                }) => {
+                    let (mut true_branch, true_required) = self.enqueue_statements(true_branch);
+                    let (mut false_branch, false_required) = self.enqueue_statements(false_branch);
+                    let intersection = true_required
+                        .intersection(&false_required)
+                        .cloned()
+                        .collect::<HashSet<_>>();
+                    let true_required = true_required.difference(&intersection).cloned();
+                    let false_required = false_required.difference(&intersection).cloned();
+                    true_branch.extend(true_required.map(|memory| Enqueue(memory).into()));
+                    true_branch.reverse();
+                    false_branch.extend(false_required.map(|memory| Enqueue(memory).into()));
+                    false_branch.reverse();
+                    required.extend(intersection);
+                    vec![IfStatement {
+                        condition: condition.clone(),
+                        branches: (true_branch, false_branch),
+                    }
+                    .into()]
+                }
                 Statement::MatchStatement(match_) => todo!(),
-                _ => vec![statement],
+                statement => vec![statement],
             })
             .collect_vec();
-        statements.reverse();
         (statements, required)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{FnCall, FnType, Id};
+    use crate::{FnCall, FnType, Id, IfStatement, MatchBranch, MatchStatement, Name, UnionType};
 
     use super::*;
 
@@ -114,13 +139,138 @@ mod tests {
         vec!["f"];
         "sequential code"
     )]
+    #[test_case(
+        vec![
+            Await(vec![Memory(Id::from("c"))]).into(),
+            IfStatement {
+                condition: Memory(Id::from("c")).into(),
+                branches: (
+                    vec![
+                        Await(vec![Memory(Id::from("f"))]).into(),
+                        Assignment{
+                            target: Memory(Id::from("y0")),
+                            value: FnCall {
+                                fn_: Memory(Id::from("f")).into(),
+                                fn_type: FnType(
+                                    vec![AtomicTypeEnum::INT.into()],
+                                    Box::new(AtomicTypeEnum::BOOL.into())
+                                ),
+                                args: vec![
+                                    Memory(Id::from("x0")).into(),
+                                ]
+                            }.into(),
+                        }.into(),
+                        Await(vec![Memory(Id::from("y0"))]).into(),
+                    ],
+                    vec![
+                        Await(vec![Memory(Id::from("f"))]).into(),
+                        Assignment{
+                            target: Memory(Id::from("y1")),
+                            value: FnCall {
+                                fn_: Memory(Id::from("f")).into(),
+                                fn_type: FnType(
+                                    vec![AtomicTypeEnum::INT.into()],
+                                    Box::new(AtomicTypeEnum::BOOL.into())
+                                ),
+                                args: vec![
+                                    Memory(Id::from("x1")).into(),
+                                ]
+                            }.into(),
+                        }.into(),
+                        Await(vec![Memory(Id::from("y1"))]).into(),
+                    ]
+                )
+            }.into()
+        ],
+        vec![
+            Await(vec![Memory(Id::from("c"))]).into(),
+            IfStatement {
+                condition: Memory(Id::from("c")).into(),
+                branches: (
+                    vec![
+                        Await(vec![Memory(Id::from("f"))]).into(),
+                        Assignment{
+                            target: Memory(Id::from("y0")),
+                            value: FnCall {
+                                fn_: Memory(Id::from("f")).into(),
+                                fn_type: FnType(
+                                    vec![AtomicTypeEnum::INT.into()],
+                                    Box::new(AtomicTypeEnum::BOOL.into())
+                                ),
+                                args: vec![
+                                    Memory(Id::from("x0")).into(),
+                                ]
+                            }.into(),
+                        }.into(),
+                        Enqueue(Memory(Id::from("y0"))).into(),
+                        Await(vec![Memory(Id::from("y0"))]).into(),
+                    ],
+                    vec![
+                        Await(vec![Memory(Id::from("f"))]).into(),
+                        Assignment{
+                            target: Memory(Id::from("y1")),
+                            value: FnCall {
+                                fn_: Memory(Id::from("f")).into(),
+                                fn_type: FnType(
+                                    vec![AtomicTypeEnum::INT.into()],
+                                    Box::new(AtomicTypeEnum::BOOL.into())
+                                ),
+                                args: vec![
+                                    Memory(Id::from("x1")).into(),
+                                ]
+                            }.into(),
+                        }.into(),
+                        Enqueue(Memory(Id::from("y1"))).into(),
+                        Await(vec![Memory(Id::from("y1"))]).into(),
+                    ]
+                )
+            }.into()
+        ],
+        vec!["f", "c"];
+        "if branches with overlap"
+    )]
+    #[test_case(
+        vec![
+            Await(vec![Memory(Id::from("c"))]).into(),
+            IfStatement {
+                condition: Memory(Id::from("c")).into(),
+                branches: (
+                    vec![
+                        Await(vec![Memory(Id::from("f"))]).into(),
+                    ],
+                    vec![
+                        Await(vec![Memory(Id::from("g"))]).into(),
+                    ]
+                )
+            }.into()
+        ],
+        vec![
+            Await(vec![Memory(Id::from("c"))]).into(),
+            IfStatement {
+                condition: Memory(Id::from("c")).into(),
+                branches: (
+                    vec![
+                        Enqueue(Memory(Id::from("f"))).into(),
+                        Await(vec![Memory(Id::from("f"))]).into(),
+                    ],
+                    vec![
+                        Enqueue(Memory(Id::from("g"))).into(),
+                        Await(vec![Memory(Id::from("g"))]).into(),
+                    ]
+                )
+            }.into()
+        ],
+        vec!["c"];
+        "if branches no overlap"
+    )]
     fn test_enqueue_statements(
         statements: Vec<Statement>,
         expected_statements: Vec<Statement>,
         expected_required_values: Vec<&str>,
     ) {
         let enqueuer = Enqueuer::new();
-        let (enqueued_statements, required_values) = enqueuer.enqueue(statements);
+        let (mut enqueued_statements, required_values) = enqueuer.enqueue_statements(statements);
+        enqueued_statements.reverse();
         assert_eq!(expected_statements, enqueued_statements);
         assert_eq!(
             expected_required_values
